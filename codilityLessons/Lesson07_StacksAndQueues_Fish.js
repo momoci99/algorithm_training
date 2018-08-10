efe4e653fb5ceccf0116b9f797906e2400537a66
jgsim <sjg4792@gmail.com>
2018-08-10 18:47:29 +0900

    add codility lesson 07 fish

--- codilityLessons/Lesson07_StacksAndQueues_Fish.js
+++ codilityLessons/Lesson07_StacksAndQueues_Fish.js
@@ -1,6 +1,31 @@
+// Lesson07_StacksAndQueues_Fish
+// https://app.codility.com/demo/results/trainingZJX4TM-ENA/
+
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
 function solution(A, B) {
     // write your code in JavaScript (Node.js 8.9.4)
+    const len = A.length;
+    const stack = [];
+    let count = 0;
+    for (let i = 0; i < len; i++) {
+        if (B[i] === 1) {
+            stack.push(A[i]);
+        } else {
+            while (stack.length > 0) {
+                let peek = stack[stack.length - 1];
+                if (peek < A[i]) {
+                    stack.pop();
+                } else {
+                    break;
+                }
+            }
+        }
+        if (stack.length === 0) {
+            count += 1;
+        }
+    }
+
+    return count + stack.length;
 }
